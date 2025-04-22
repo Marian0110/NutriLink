ff5fe725a3c9b791473ed0c534d192dd78ca4ef0
Mariana <mariianaf3@gmail.com>
2025-04-22 12:08:18 -0400

    Implementando registro con Api

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,8 +1,47 @@
-from django.shortcuts import render
-
+import requests
+from django.shortcuts import render, redirect
+from django.contrib import messages
 # Create your views here.
 
 def registro(request):
+    if request.method == 'POST':
+        # Traer datos del formulario
+        rut_completo = request.POST.get('rut', '').replace('.', '').replace('-', '')
+        rut_numerico = rut_completo[:-1]
+        dv = rut_completo[-1].upper()
+        
+        data = {
+            "rut_nutricionista": rut_numerico,
+            "dv": dv,
+            "primer_nombre": request.POST.get('primerNombre'),
+            "apellido_paterno": request.POST.get('apellidoP'),
+            "correo": request.POST.get('email'),
+            "contrasena": request.POST.get('contraseña'),
+            # Campos opcionales explicitamente como None si son vacios
+            "segundo_nombre": None,
+            "apellido_materno": None,
+            "fecha_nacimiento": None,
+            "foto": None,
+            "telefono": None
+        }
+
+        try:
+            response = requests.post(
+                "https://nutrilinkapi-production.up.railway.app/api_nutrilink/nutricionista/registrar",
+                json=data,
+                timeout=4
+            )
+
+            if response.status_code == 201:
+                messages.success(request, '¡Registro exitoso!')
+                return redirect('accounts:perfil')
+            else:
+                error_msg = response.json().get('error', 'Error desconocido')
+                messages.error(request, f'Error: {error_msg}')
+                
+        except requests.exceptions.RequestException as e:
+            messages.error(request, 'Error al conectar con el servidor')
+
     return render(request, 'accounts/registro.html')
 
 def login(request):
@@ -13,9 +52,3 @@ def perfil(request):
 
 def redirectPerfil(request):
     return render(request, 'accounts/perfil.html') #por el momento
-
-# def registro(request):
-#     if request.method == 'POST':
-#         # En el futuro manejo de usuario
-#         return redirect('perfil')
-#     return render(request, 'accounts/registro.html')
