c70ae95daa2bfacad371f9331f943529f12e7acb
Mariana <mariianaf3@gmail.com>
2025-04-23 19:20:10 -0400

    Modificación de rutas

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('registro/', views.registro, name='registro'),
     path('login/', views.login, name='login'),
     path('perfil/', views.perfil, name='perfil'),
-    path('redirect/', views.redirectPerfil, name='redirectPerfil'),
+    path('logout/', views.logout, name='logout'),
 
 ]
