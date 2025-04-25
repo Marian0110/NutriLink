39bf8872bab4b0120b49425f2c9f37f58470cba9
Mariana <mariianaf3@gmail.com>
2025-04-25 13:00:20 -0400

    Se agrega paginación de alimentos

--- apps/foods/views.py
+++ apps/foods/views.py
@@ -1,11 +1,6 @@
 from django.shortcuts import render
 import requests
-from django.http import JsonResponse
-
-# Create your views here.
-
-from django.shortcuts import render
-import requests
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def bdCrud(request):
     url = 'https://nutrilinkapi-production.up.railway.app/api_nutrilink/alimento/alimentos'
@@ -18,7 +13,7 @@ def bdCrud(request):
             data = response.json()
             alimentos = data
 
-            # Recolectar los grupos únicos
+            # Recolectar los grupos uniques
             grupos = sorted(set(item.get('nombre_grupo_alimenticio', '') for item in data if item.get('nombre_grupo_alimenticio')))
             # Filtros
             query = request.GET.get('q', '').lower()
@@ -28,15 +23,24 @@ def bdCrud(request):
                 alimentos = [a for a in alimentos if query in a.get('nombre_alimento', '').lower()]
             if grupo_filtro:
                 alimentos = [a for a in alimentos if a.get('nombre_grupo_alimenticio') == grupo_filtro]
-                
+
+            # Paginacion - 5 elementos por pg
+            paginator = Paginator(alimentos, 5)
+            page_number = request.GET.get('page')
+            try:
+                page_obj = paginator.page(page_number)
+            except PageNotAnInteger:
+                page_obj = paginator.page(1)
+            except EmptyPage:
+                page_obj = paginator.page(paginator.num_pages)
 
     except requests.exceptions.RequestException as e:
         print("Error:", e)
+        page_obj = None
 
     context = {
-        'alimentos': alimentos,
+        'alimentos': alimentos, 
+        'page_obj': page_obj, #obj de paginacion
         'grupos': grupos
     }
     return render(request, 'foods/bdCrud.html', context)
-
-
